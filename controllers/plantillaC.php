54a8cfe1238d306bd0de819b623e4c3cd0ccb11c
fwan099 <freddy.hu099@gmail.com>
2022-06-30 11:57:21 -0500

    se agrega el proyecto

---
+++ controllers/plantillaC.php
@@ -0,0 +1,11 @@
+<?php
+
+class PlantillaC{
+
+	public function LlamarPlantilla(){
+
+		include "views/plantilla.html";
+
+	} 
+
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "controllers/plantillaC.php";
+
+$plantilla = new PlantillaC();
+$plantilla -> LlamarPlantilla();
