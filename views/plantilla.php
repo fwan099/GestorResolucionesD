eb694fdce1ee06c050afdcdb320402dc33e26bf8
fwan099 <freddy.hu099@gmail.com>
2022-06-30 20:50:30 -0500

    visual 2

--- views/plantilla.php
+++ views/plantilla.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de Sesion</title>
     <link href="https://fonts.googleapis.com/css2?family=Mukta&family=Roboto+Mono:wght@100;200&family=Roboto:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./../public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./../public/css/estilos.css">
+    <link rel="stylesheet" href="./public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./public/css/estilos.css">
     
 </head>
 <body>
