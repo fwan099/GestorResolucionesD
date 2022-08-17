1b7301f07c5bee07ef5975ea3398cfeff01bf23c
fwan099 <freddy.hu099@gmail.com>
2022-08-17 18:34:47 -0500

    bd actualizada

--- ugel_rd.sql
+++ ugel_rd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-08-2022 a las 17:19:29
+-- Tiempo de generación: 18-08-2022 a las 01:33:59
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
@@ -24,19 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `personal`
---
-
-CREATE TABLE `personal` (
-  `id_personal` int(11) NOT NULL,
-  `nombres` varchar(45) DEFAULT NULL,
-  `apellido_pat` varchar(45) DEFAULT NULL,
-  `dni` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `resolucion`
 --
 
@@ -44,10 +31,20 @@ CREATE TABLE `resolucion` (
   `id_resolucion` int(11) NOT NULL,
   `fecha` date DEFAULT NULL,
   `doc_url` varchar(45) DEFAULT NULL,
-  `id_personal` int(11) NOT NULL,
   `id_tipo_resolucion` int(11) NOT NULL,
-  `numero_res` varchar(45) DEFAULT NULL
+  `numero_res` varchar(45) DEFAULT NULL,
+  `dni` varchar(45) DEFAULT NULL,
+  `nombres` varchar(45) DEFAULT NULL,
+  `paterno` varchar(45) DEFAULT NULL,
+  `materno` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `resolucion`
+--
+
+INSERT INTO `resolucion` (`id_resolucion`, `fecha`, `doc_url`, `id_tipo_resolucion`, `numero_res`, `dni`, `nombres`, `paterno`, `materno`) VALUES
+(17, '2022-08-17', 'views/public/docs/O457.pdf', 21, '1', '73744393', 'FREDDY WALTER', 'HUAYNAPATA', 'UCHARICO');
 
 -- --------------------------------------------------------
 
@@ -59,6 +56,13 @@ CREATE TABLE `tipo_resolucion` (
   `id_tipo_resolucion` int(11) NOT NULL,
   `detalle` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tipo_resolucion`
+--
+
+INSERT INTO `tipo_resolucion` (`id_tipo_resolucion`, `detalle`) VALUES
+(21, 'CONCERDER LICENCIA POR MATERNIDAD CON GOCE DE REMUNERACION');
 
 -- --------------------------------------------------------
 
@@ -85,17 +89,10 @@ INSERT INTO `usuario` (`id_usuario`, `usuario`, `clave`, `rol`) VALUES
 --
 
 --
--- Indices de la tabla `personal`
---
-ALTER TABLE `personal`
-  ADD PRIMARY KEY (`id_personal`);
-
---
 -- Indices de la tabla `resolucion`
 --
 ALTER TABLE `resolucion`
   ADD PRIMARY KEY (`id_resolucion`),
-  ADD KEY `fk_resolucion_personal` (`id_personal`),
   ADD KEY `fk_resolucion_tipo_resolucion1` (`id_tipo_resolucion`);
 
 --
@@ -115,28 +112,22 @@ ALTER TABLE `usuario`
 --
 
 --
--- AUTO_INCREMENT de la tabla `personal`
---
-ALTER TABLE `personal`
-  MODIFY `id_personal` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `resolucion`
 --
 ALTER TABLE `resolucion`
-  MODIFY `id_resolucion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_resolucion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_resolucion`
 --
 ALTER TABLE `tipo_resolucion`
-  MODIFY `id_tipo_resolucion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tipo_resolucion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -146,7 +137,6 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `resolucion`
 --
 ALTER TABLE `resolucion`
-  ADD CONSTRAINT `fk_resolucion_personal` FOREIGN KEY (`id_personal`) REFERENCES `personal` (`id_personal`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_resolucion_tipo_resolucion1` FOREIGN KEY (`id_tipo_resolucion`) REFERENCES `tipo_resolucion` (`id_tipo_resolucion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
